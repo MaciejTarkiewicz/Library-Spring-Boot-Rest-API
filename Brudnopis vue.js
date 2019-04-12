35649e19849b1d5746885b3e41733054b247eee6
MaciejTarkiewicz <47443988+MaciejTarkiewicz@users.noreply.github.com>
2019-04-12 15:32:45 +0200

    Update Brudnopis vue.js

--- Brudnopis vue.js
+++ Brudnopis vue.js
@@ -1,87 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="main.css" media="screen"/>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="vue.js"></script>
+</head>
+
+
+<body>
 <div id="app">
-  <h2>Registration:</h2>
-  <ol>
-    <li v-for="page in pages">
-      <label>
-        <input type="text"  name = page.text placeholder="edit me"
-          v-model="message" >
-          
-          <span>
+    {{ test }}
+    <h2>Registration:</h2>
+    <ol>
+        <li v-for="page in pages">
+            <label>
+                <input type="text"  name = page.text placeholder="edit me"
+                       v-model="message" >
+                <span>
           {{ page.text }}
         </span>
-       
-      </label>
-    </li>
-    <button type="submit" value="Submit">
-    Register
-    </button>
-  </ol>
-  <h2>
-  
-  </h2>
-   <h2>Sign up:</h2>
-   <ol>
-    <li v-for="item in loginPage">
-     <label>
-       <input type="text"  name = item.text placeholder="edit me"
-          v-model="message" >
-          
-          <span>
-          {{ item.text }}
+
+            </label>
+        </li>
+        <button type="submit" value="Submit">
+            Register
+        </button>
+    </ol>
+    <h2>
+    </h2>
+    <h2>Sign up:</h2>
+    <ol>
+        <li v-for="item in loginPage">
+            <label>
+                <input type="text"  name = item.text placeholder="edit me"
+                       v-model="message" >
+                <span>
+          {{item.text}}
         </span>
-        
-     </label>     
-    </li>
-   </ol>
-   
-   <button type="submit" value="Submit">
-    Sign
+
+            </label>
+        </li>
+    </ol>
+
+    <button type="submit" value="Submit">
+        Sign
     </button>
 </div>
+</body>
+</html>
 
 
-new Vue({
-  el: "#app",
-  data: {
-    pages: [
-      { text: "Username" },
-      { text: "Password" },
-      { text: "Confirm Password"},
-      { text: "Email"},
-      { text: "Confirm Email"}
-    	],
-    loginPage: [
-      { text: "Username"},
-      { text: "Password"},
-    	]
-    
-  },
 
-})
+window.onload = () => {
+    new Vue({
+        el: "#app",
+        data: {
+            pages: [
+                {text: "Username"},
+                {text: "Password"},
+                {text: "Confirm Password"},
+                {text: "Email"},
+                {text: "Confirm Email"}
+            ],
+            loginPage: [
+                {text: "Username"},
+                {text: "Password"},
+            ]
+
+        },
+    })
+
+}
 
 
 body {
-  background: #20262E;
-  padding: 20px;
-  font-family: Helvetica;
+    background: #20262E;
+    padding: 20px;
+    font-family: Helvetica;
 }
 
 #app {
-  background: #fff;
-  border-radius: 4px;
-  padding: 20px;
-  transition: all 0.2s;
+    background: #fff;
+    border-radius: 4px;
+    padding: 20px;
+    transition: all 0.2s;
 }
 
 li {
-  margin: 8px 0;
+    margin: 8px 0;
 }
 
 h2 {
-  font-weight: bold;
-  margin-bottom: 15px;
+    font-weight: bold;
+    margin-bottom: 15px;
 }
 
 del {
-  color: rgba(0, 0, 0, 0.3);
+    color: rgba(0, 0, 0, 0.3);
 }
